34e24bd34c381ef5487a892bfc9d6d3f5b37805c
jion08 <100652348+jion08@users.noreply.github.com>
2025-11-21 22:45:31 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
 
+// firebase-config.js
+// 여기 안에 네 Firebase 설정을 넣어줘.
+
+const firebaseConfig = {
+ apiKey: "AIzaSyBntPXzM70l6u-i3NbfBDBn_6LdK9cg5yQ",
+authDomain: "community-bb416.firebaseapp.com",
+projectId: "community-bb416",
+storageBucket: "community-bb416.firebasestorage.app",
+messagingSenderId: "1087414318672",
+appId: "1:1087414318672:web:7131f332b1d2a9c14fc355",
+};
